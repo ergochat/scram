a922a175bb2ba136f1fd248f05922041b2b5ac9c
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2021-07-29 17:27:08 -0400

    remove stringprep
    Reduces binary size

--- scram.go
+++ scram.go
@@ -9,10 +9,7 @@ package scram
 import (
 	"crypto/sha1"
 	"crypto/sha256"
-	"fmt"
 	"hash"
-
-	"github.com/xdg-go/stringprep"
 )
 
 // HashGeneratorFcn abstracts a factory function that returns a hash.Hash
@@ -28,27 +25,6 @@ var SHA1 HashGeneratorFcn = func() hash.Hash { return sha1.New() }
 // SHA256 is a function that returns a crypto/sha256 hasher and should be used
 // to create Client objects configured for SHA-256 hashing.
 var SHA256 HashGeneratorFcn = func() hash.Hash { return sha256.New() }
-
-// NewClient constructs a SCRAM client component based on a given hash.Hash
-// factory receiver.  This constructor will normalize the username, password
-// and authzID via the SASLprep algorithm, as recommended by RFC-5802.  If
-// SASLprep fails, the method returns an error.
-func (f HashGeneratorFcn) NewClient(username, password, authzID string) (*Client, error) {
-	var userprep, passprep, authprep string
-	var err error
-
-	if userprep, err = stringprep.SASLprep.Prepare(username); err != nil {
-		return nil, fmt.Errorf("Error SASLprepping username '%s': %v", username, err)
-	}
-	if passprep, err = stringprep.SASLprep.Prepare(password); err != nil {
-		return nil, fmt.Errorf("Error SASLprepping password '%s': %v", password, err)
-	}
-	if authprep, err = stringprep.SASLprep.Prepare(authzID); err != nil {
-		return nil, fmt.Errorf("Error SASLprepping authzID '%s': %v", authzID, err)
-	}
-
-	return newClient(userprep, passprep, authprep, f), nil
-}
 
 // NewClientUnprepped acts like NewClient, except none of the arguments will
 // be normalized via SASLprep.  This is not generally recommended, but is
